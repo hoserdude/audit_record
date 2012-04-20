7c094f67f9c4cc747ec9ea56a02b7c37c2747f4f
Mark Daggett <mark@humansized.com>
2012-04-20 15:20:44 -0500

    needed to check for the existence of current_user too.

--- lib/auditable/audit_record_sweeper.rb
+++ lib/auditable/audit_record_sweeper.rb
@@ -1,7 +1,7 @@
 class AuditRecordSweeper < ActionController::Caching::Sweeper
   observe AuditRecord
   def before_create(audit_record)
-    audit_record.user_id ||= current_user.id
+    audit_record.user_id ||= current_user.id unless current_user.nil?
     audit_record.remote_address = controller.try(:request).try(:ip)
   end
 end
